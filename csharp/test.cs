14c6bfc037fe4c631a11351529f542034ec41609
Armand Thierry DJAPPI <djappino@student.tut.fi>
2024-03-01 20:16:33 +0200

    Add files via upload

---
+++ csharp/test.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ExampleNamespace
+{
+    class ExampleClass
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine("The sum of " + x + " and " + y + " is " + (x + y));
+        }
+    }
+}
